ed234a89f2118c4ca6bd959bbbc900ad51c75ff4
KeyPJ <pengjian1994@gmail.com>
2022-04-17 10:39:32 +0800

    feat: drag scroll for desktop

--- src/components/BannersShow.tsx
+++ src/components/BannersShow.tsx
@@ -2,6 +2,8 @@ import {gachaData, Item} from "genshin-wishes";
 import moment from "moment";
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
+import ScrollContainer from 'react-indiana-drag-scroll'
+import {isMobile} from 'react-device-detect';
 
 interface IProps {
     rankType: number[],
@@ -57,109 +59,113 @@ export default function BannersShow(props: IProps) {
         setCurrentGachaItemId(itemIds.toString() == currentGachaItemId.toString() ? [] : itemIds)
     }
 
-    return (
-        <div className="text-center flex flex-col min-w-screen h-fit overflow-x-auto overflow-hidden overscroll-x-auto">
-            {columnItems.length > 0 && columnItems.slice(-1).concat(columnItems)
-                .map((item, index) => {
-                    if (index == 0) {
-                        return <div key={index} className={"flex flex-row shrink-0 w-fit h-fit"}>
-                            <div
-                                className={classNames(itemClassName, "sticky left-0 bg-white z-10 text-sm whitespace-pre-line")}
-                                onClick={
-                                    () => {
-                                        setSortB(1);
-                                        setCurrentGachaItemId([])
-                                    }
-                                }
-                            >
-                                {t("rowClick")}
-                            </div>
-                            <div
-                                className={classNames(itemClassName, "sticky left-20 bg-white z-10 text-sm whitespace-pre-line")}
-                                onClick={
-                                    () => setSortB(sortB == 0 ? 1 : 0)
-                                }>
-                                {sortB == 0 ? t("unavailable") : t("release")}
-                            </div>
-                            {data.map(gacha => {
-                                    let [start, end] = gacha.startEndByRegion.ASIA;
-                                    return (
-                                        <div key={`${index}-${gacha.id}`}
-                                             className={classNames(itemClassName, "text-center text-sm cursor-pointer", gacha.items.map(i => i.itemId).toString() == currentGachaItemId.toString() ? "ring-2 border-indigo-500" : "")}
-                                             onClick={() => handleGachaClick(gacha.items.map(i => i.itemId))}
-                                        >
-                                            {gacha.version}<br/>
-                                            {dateFormat(start)}<br/>
-                                            {dateFormat(end)}<br/>
-                                            {/*<img src={imageBaseUrl + gacha.image.url} alt={gacha.version}*/}
-                                            {/*     className={classNames("border-solid rounded-1 hover:fixed hover:inset-x-0 hover:m-auto hover:z-20")}/>*/}
-                                        </div>
-                                    )
-                                }
-                            )}
-                        </div>
-                    }
-                    const borderColor = item.rankType == 5 ? "border-amber-500" : "border-purple-500";
-                    let tempNumber: number = -1;
-                    let findIndex = data.map(gacha => gacha.items.map(i => i.itemId).includes(item.itemId)).reverse().findIndex(b => b);
-                    let pickUpGacha = data[data.length - 1 - findIndex];
-                    let days = Math.floor(moment.duration(moment().diff(moment(pickUpGacha.end))).asDays());
-                    return (<div key={index} className={"flex flex-row shrink-0 w-fit"}>
-                            <div className={classNames(itemClassName, "sticky left-0 bg-white ")}>
-                                <img src={imageBaseUrl + item.image.url} alt={item.name}
-                                     className={`${itemClassName} ${borderColor} border-solid rounded-[50%]`}/>
-                            </div>
-                            <div
-                                className={classNames(itemClassName, "sticky left-20 bg-white z-10 text-xs whitespace-pre-line")}>
-                                {
-                                    item.rankType == 5 && !commonItemId.includes(item.itemId) ?
-                                        (findIndex == 0 ? <div>{t("pickUp")}</div> :
-                                            <div>
-                                                {t("sinceLastPickUp", {findIndex, days})}
-                                            </div>)
-                                        : (findIndex == 0 ? <div>
-                                            {t("permanent")}<br/>
-                                            {t("pickUp")}
-                                        </div> :
-                                        <div>
-                                            {t("permanent")}<br/>
-                                            {t("sinceLastPickUp", {findIndex, days})}
-                                        </div>)
-                                }
-                            </div>
-                            {data.map(gacha => {
-                                    if (gacha.items.map(i => i.itemId).includes(item.itemId)) {
-                                        tempNumber = 0;
-                                        return (
-                                            <div key={`${item.itemId}-${gacha.id}`}
-                                                 className={classNames(itemClassName)}>
-                                                <img src={imageBaseUrl + item.image.url} alt={item.name}
-                                                     className={`${itemClassName} ${borderColor} border-solid rounded-[50%]`}/>
-                                            </div>
-                                        )
-                                    } else {
-                                        tempNumber = tempNumber >= 0 ? tempNumber + 1 : tempNumber;
-
-                                        let style = {};
-                                        if (tempNumber > 0) {
-                                            let to = 255 - 255 / findIndexMax * tempNumber;
-                                            let from = 255 - 255 / findIndexMax * (tempNumber + 1);
-                                            style = {backgroundImage: `linear-gradient(to right, rgb(${from}, ${from}, 255) , rgb(${to}, ${to}, 255))`}
-                                        }
-
-                                        return <div key={`${item.itemId}-${gacha.id}`}
-                                                    className={classNames(itemClassName, "text-3xl font-bold leading-loose")}
-                                                    style={style}
-                                        >
-                                            {tempNumber > 0 ? tempNumber : ""}
-                                        </div>;
-                                    }
-                                }
-                            )}
-                        </div>
-                    )
-                })
+    const flexRows = <>{columnItems.length > 0 && columnItems.slice(-1).concat(columnItems)
+        .map((item, index) => {
+            if (index == 0) {
+                return <div key={index} className={"flex flex-row shrink-0 w-fit h-fit sticky top-0"}>
+                    <div
+                        className={classNames(itemClassName, "sticky left-0 bg-white z-10 text-sm whitespace-pre-line")}
+                        onClick={
+                            () => {
+                                setSortB(1);
+                                setCurrentGachaItemId([])
+                            }
+                        }
+                    >
+                        {t("rowClick")}
+                    </div>
+                    <div
+                        className={classNames(itemClassName, "sticky left-20 bg-white z-10 text-sm whitespace-pre-line")}
+                        onClick={
+                            () => setSortB(sortB == 0 ? 1 : 0)
+                        }>
+                        {sortB == 0 ? t("unavailable") : t("release")}
+                    </div>
+                    {data.map(gacha => {
+                            let [start, end] = gacha.startEndByRegion.ASIA;
+                            return (
+                                <div key={`${index}-${gacha.id}`}
+                                     className={classNames(itemClassName, "text-center text-sm cursor-pointer", gacha.items.map(i => i.itemId).toString() == currentGachaItemId.toString() ? "ring-2 border-indigo-500" : "")}
+                                     onClick={() => handleGachaClick(gacha.items.map(i => i.itemId))}
+                                >
+                                    {gacha.version}<br/>
+                                    {dateFormat(start)}<br/>
+                                    {dateFormat(end)}<br/>
+                                    {/*<img src={imageBaseUrl + gacha.image.url} alt={gacha.version}*/}
+                                    {/*     className={classNames("border-solid rounded-1 hover:fixed hover:inset-x-0 hover:m-auto hover:z-20")}/>*/}
+                                </div>
+                            )
+                        }
+                    )}
+                </div>
             }
+            const borderColor = item.rankType == 5 ? "border-amber-500" : "border-purple-500";
+            let tempNumber: number = -1;
+            let findIndex = data.map(gacha => gacha.items.map(i => i.itemId).includes(item.itemId)).reverse().findIndex(b => b);
+            let pickUpGacha = data[data.length - 1 - findIndex];
+            let days = Math.floor(moment.duration(moment().diff(moment(pickUpGacha.end))).asDays());
+            return (<div key={index} className={"flex flex-row shrink-0 w-fit"}>
+                    <div className={classNames(itemClassName, "sticky left-0 bg-white ")}>
+                        <img src={imageBaseUrl + item.image.url} alt={item.name}
+                             className={`${itemClassName} ${borderColor} border-solid rounded-[50%]`}/>
+                    </div>
+                    <div
+                        className={classNames(itemClassName, "sticky left-20 bg-white z-10 text-xs whitespace-pre-line")}>
+                        {
+                            item.rankType == 5 && !commonItemId.includes(item.itemId) ?
+                                (findIndex == 0 ? <div>{t("pickUp")}</div> :
+                                    <div>
+                                        {t("sinceLastPickUp", {findIndex, days})}
+                                    </div>)
+                                : (findIndex == 0 ? <div>
+                                    {t("permanent")}<br/>
+                                    {t("pickUp")}
+                                </div> :
+                                <div>
+                                    {t("permanent")}<br/>
+                                    {t("sinceLastPickUp", {findIndex, days})}
+                                </div>)
+                        }
+                    </div>
+                    {data.map(gacha => {
+                            if (gacha.items.map(i => i.itemId).includes(item.itemId)) {
+                                tempNumber = 0;
+                                return (
+                                    <div key={`${item.itemId}-${gacha.id}`}
+                                         className={classNames(itemClassName)}>
+                                        <img src={imageBaseUrl + item.image.url} alt={item.name}
+                                             className={`${itemClassName} ${borderColor} border-solid rounded-[50%]`}/>
+                                    </div>
+                                )
+                            } else {
+                                tempNumber = tempNumber >= 0 ? tempNumber + 1 : tempNumber;
+
+                                let style = {};
+                                if (tempNumber > 0) {
+                                    let to = 255 - 255 / findIndexMax * tempNumber;
+                                    let from = 255 - 255 / findIndexMax * (tempNumber + 1);
+                                    style = {backgroundImage: `linear-gradient(to right, rgb(${from}, ${from}, 255) , rgb(${to}, ${to}, 255))`}
+                                }
+
+                                return <div key={`${item.itemId}-${gacha.id}`}
+                                            className={classNames(itemClassName, "text-3xl font-bold leading-loose")}
+                                            style={style}
+                                >
+                                    {tempNumber > 0 ? tempNumber : ""}
+                                </div>;
+                            }
+                        }
+                    )}
+                </div>
+            )
+        })
+    }</>;
+    return (
+        <div
+            className="text-center flex flex-col min-w-screen min-w-screen overflow-x-auto overflow-hidden overscroll-x-auto">
+            {isMobile ? flexRows : <ScrollContainer vertical={false}>
+                {flexRows}
+            </ScrollContainer>}
         </div>
     )
 }
