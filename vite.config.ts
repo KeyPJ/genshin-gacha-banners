923bb539fbd9f242cdad5d4aae3fa9e6cdd57278
KeyPJ <pengjian1994@gmail.com>
2023-04-12 08:06:46 +0800

    feat: split Data

--- vite.config.ts
+++ vite.config.ts
@@ -83,6 +83,11 @@ export default defineConfig({
                 rewrite: path => path.replace(/^\/game_record/, ''), // 重写传过来的path路径，比如 `/api/index/1?id=10&name=zs`（注意:path路径最前面有斜杠（/），因此，正则匹配的时候不要忘了是斜杠（/）开头的；选项的 key 也是斜杠（/）开头的）
                 changeOrigin: true,  // true/false, Default: false - changes the origin of the host header to the target URL
             },
+            '/data': {
+                target: 'https://raw.githubusercontent.com/KeyPJ/FetchData/main/data/gacha/', // 所要代理的目标地址
+                rewrite: path => path.replace(/^\/data/, ''), // 重写传过来的path路径，比如 `/api/index/1?id=10&name=zs`（注意:path路径最前面有斜杠（/），因此，正则匹配的时候不要忘了是斜杠（/）开头的；选项的 key 也是斜杠（/）开头的）
+                changeOrigin: true,  // true/false, Default: false - changes the origin of the host header to the target URL
+            },
             // 选项写法
             // '/api': {
             //     target: 'https://genshin-wishes.com/api', // 所要代理的目标地址
